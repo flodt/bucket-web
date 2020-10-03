586654e381eeddd6922b49f1591423d3a7f5a41e
Florian Schmidt <fs.schmidt@tum.de>
2020-10-03 12:01:14 +0200

    remove unused function

--- src/components/Share.vue
+++ src/components/Share.vue
@@ -258,28 +258,4 @@ function uuidv4() {
         (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
     );
 }
-
-/**
- * Sourced from https://stackoverflow.com/questions/14071463/how-can-i-merge-typedarrays-in-javascript.
- */
-function mergeTypedArrays(a, b) {
-    // Checks for truthy values on both arrays
-    if (!a && !b) throw 'Please specify valid arguments for parameters a and b.';
-
-    // Checks for truthy values or empty arrays on each argument
-    // to avoid the unnecessary construction of a new array and
-    // the type comparison
-    if (!b || b.length === 0) return a;
-    if (!a || a.length === 0) return b;
-
-    // Make sure that both typed arrays are of the same type
-    if (Object.prototype.toString.call(a) !== Object.prototype.toString.call(b))
-        throw 'The types of the two arguments passed for parameters a and b do not match.';
-
-    const c = new a.constructor(a.length + b.length);
-    c.set(a);
-    c.set(b, a.length);
-
-    return c;
-}
 </script>
